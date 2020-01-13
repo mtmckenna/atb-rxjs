5064e66b3e4111886106228aaf1ea0d67285bd1b
Matt McKenna <matt@mtmckenna.com>
2020-01-12 20:45:02 -0800

    add sprites

--- index.js
+++ index.js
@@ -14,20 +14,19 @@ import {
 } from "rxjs/operators";
 
 const values = {
-    characters: [
-        { hp: 1250, mp: 75, wait: 10 },
-        { hp: 475, mp: 200, wait: 90 },
-        { hp: 750, mp: 120, wait: 0 }
+    heros: [
+        { maxHp: 1500, hp: 1250, mp: 75, wait: 10 },
+        { maxHp: 600,  hp: 475, mp: 200, wait: 90 },
+        { maxHp: 250,  hp: 750, mp: 120, wait: 0 }
     ]
 };
 
-const primaryMenu = document.getElementById("primary-menu");
 const secondaryMenu = document.getElementById("secondary-menu");
 const secondaryBack = document.getElementById("secondary-back");
 
 const waitEls = Array(3).fill().map((_, i) => {
     return document
-        .getElementById(`character-${i}-stats`)
+        .getElementById(`hero-${i}-stats`)
         .getElementsByClassName("progress-bar")[0];
 });
 
@@ -35,39 +34,60 @@ const waitFillingEls = Array(3).fill().map((_, i) => {
     return waitEls[i].getElementsByClassName("progress-bar-filling")[0];
 });
 
-const characterEls = Array(3).fill().map((_, i) => {
-    return document.getElementById(`character-name-${i}`);
+const heroEls = Array(3).fill().map((_, i) => {
+    return document.getElementById(`hero-name-${i}`);
+});
+
+const hpEls = Array(3).fill().map((_, i) => {
+    return document
+    .getElementById(`hero-${i}-stats`)
+    .getElementsByClassName("hp")[0];
+});
+
+const mpEls = Array(3).fill().map((_, i) => {
+    return document
+    .getElementById(`hero-${i}-stats`)
+    .getElementsByClassName("mp")[0];
+});
+
+const heroSpriteEls = Array(3).fill().map((_, i) => {
+    return document
+    .getElementById(`hero-${i}`)
+    .getElementsByClassName("sprite")[0];
 });
 
 const clock$ = interval(0, animationFrameScheduler).pipe(share());
 
 // Add to the wait timer
 const waits$ = Array(3).fill().map((_, i) => {
-    const character = values.characters[i];
-    return clock$.pipe(map(() => Math.min(character.wait + .25, 100)));
+    const hero = values.heros[i];
+    return clock$.pipe(map(() => Math.min(hero.wait + .25, 100)));
 });
 
 const selectedCharater$ = new Subject();
 
-const characterClick$ = Array(3).fill().map((_, i) => {
-    const el = characterEls[i];
-    const character = values.characters[i];
+const heroClick$ = Array(3).fill().map((_, i) => {
+    const el = heroEls[i];
+    const elSprite = heroSpriteEls[i];
+    const hero = values.heros[i];
     const wait$ = waits$[i]
-    return fromEvent(el, "click").pipe(
+    return fromEvent([el, elSprite], "click").pipe(
         withLatestFrom(wait$),
         filter(([_, wait]) => wait === 100),
-        tap(() => {
-            selectedCharater$.next(character)
-        })
+        tap(() => selectedCharater$.next(hero))
     )
 });
 
 selectedCharater$.pipe(
-    tap((character) => {
-        characterEls.forEach((el) => setSelected(el, false));
-        if (character) {
-            const el = characterEls[values.characters.indexOf(character)];
+    tap((hero) => {
+        heroEls.forEach((el) => setSelected(el, false));
+        heroSpriteEls.forEach((el) => setSelected(el, false));
+
+        if (hero) {
+            const el = heroEls[values.heros.indexOf(hero)];
+            const spriteEl = heroSpriteEls[values.heros.indexOf(hero)];
             setSelected(el, true);
+            setSelected(spriteEl, true);
             show(secondaryMenu);
         } else {
             hide(secondaryMenu);
@@ -75,28 +95,27 @@ selectedCharater$.pipe(
     })
 ).subscribe();
 
-fromEvent(secondaryBack, "click").pipe(
-    tap(() => selectedCharater$.next(null))
-).subscribe();
+fromEvent(secondaryBack, "click").pipe(tap(() => selectedCharater$.next(null))).subscribe();
 
 fromEvent(document, "click").pipe(
     filter(event => event.target.classList.contains("action")),
     withLatestFrom(selectedCharater$),
-    tap(([_, character]) => {
-        character.wait = 0;
+    tap(([_, hero]) => {
+        hero.wait = 0;
         selectedCharater$.next(null);
     })
 ).subscribe();
 
-const characterSubs = Array(3).fill().map((_, i) => {
-    return characterClick$[i].subscribe();
+const heroSubs = Array(3).fill().map((_, i) => {
+    return heroClick$[i].subscribe();
 });
 
-// Character name is no longer grayed out when timer is full
+// hero name is no longer grayed out when timer is full
 const waitSubs = waits$.map((wait$, i) =>{
     return wait$.subscribe((timer) => {
-        setReady(characterEls[i], timer === 100);
-        values.characters[i].wait = timer;
+        setReady(heroEls[i], timer === 100);
+        setReady(heroSpriteEls[i], timer === 100);
+        values.heros[i].wait = timer;
     });
 })
 
@@ -128,9 +147,9 @@ function show(el) {
 
 function draw() {
     requestAnimationFrame(draw);
-    waitFillingEls.forEach((el, i) => {
-        el.style.width = `${values.characters[i].wait}%`;
-    });
+    waitFillingEls.forEach((el, i) => el.style.width = `${values.heros[i].wait}%`);
+    hpEls.forEach((el, i) => el.textContent = `${values.heros[i].hp} / ${values.heros[i].maxHp}`);
+    mpEls.forEach((el, i) => el.textContent = `${values.heros[i].mp}`);
 }
 
 requestAnimationFrame(draw)
