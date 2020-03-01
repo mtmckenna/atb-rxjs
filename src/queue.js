a9c7986b3079ed63a9a8e5d0f8c6ff74c04a14b6
Matt McKenna <matt@mtmckenna.com>
2020-02-29 21:36:43 -0800

    fix bug where timer would tick forward even if enemy was animating

--- src/queue.js
+++ src/queue.js
@@ -1,10 +1,13 @@
-import { BehaviorSubject, defer, from, forkJoin, Subject, EMPTY, concat } from "rxjs";
-import { concatAll, scan, share, tap } from "rxjs/operators";
+import { BehaviorSubject, Subject, ReplaySubject } from "rxjs";
+import { concatAll, scan, share, shareReplay } from "rxjs/operators";
 
 export default class Queue {
   constructor() {
     this.queue$ = new Subject().pipe(concatAll(), share());
-    this.size$ = new BehaviorSubject(0).pipe(scan((sum, next) => sum + next, 0));
+    this.size$ = new BehaviorSubject(0).pipe(
+      scan((sum, next) => sum + next, 0),
+      shareReplay(1)
+    );
     this.queuedSources = [];
     this.queue$.subscribe(() => {
       this.size$.next(-1);
